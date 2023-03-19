e4173579df6522624c9179d091c5373896b10382
programist78 <pavelmolodecfda@gmail.com>
2023-03-19 20:07:23 +0200

    +first reels

---
+++ Text/1/index.jsx
@@ -0,0 +1,9 @@
+import styles from './styles.module.scss'
+
+export default function Hello() {
+  return (
+    <div className={styles.back}>
+        <p className={styles.title}>Lorem ipsum dolor sit amet.</p>
+    </div>
+  )
+}
